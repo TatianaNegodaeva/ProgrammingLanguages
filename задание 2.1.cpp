f26b0240c20af10228961689d7da0900fbd44763
TatianaNegodaeva <negodaeva.tanya2005@gmail.com>
2022-09-27 13:09:39 +0300

    я старалась

---
+++ задание 2.1.cpp
@@ -0,0 +1,14 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+int main()
+{
+	int r;
+	std::cin >> r;
+	std::cout << "V = " << (4 * M_PI * pow(r, 3)) / 3 << ", S = " << 4 * M_PI * pow(r, 2);
+}
+
+/*
+*brief 
+*/
